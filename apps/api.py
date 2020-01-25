9cbed29d104dda70a61c9e56c73c1ba669e069d6
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2020-01-25 01:56:04 -0300

    Introdução, configuração e Hello World

---
+++ apps/api.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Importamos as classes API e Resource
+from flask_restful import Api, Resource
+
+# Criamos uma classe que extende de Resource
+class Index(Resource):
+
+    # Definimos a operação get do protocolo http
+    def get(self):
+        
+        # retornamos um simples dicionário que será automaticamente 
+        # retornado em json pelo flask
+        return {'hello': 'world by apps'}
+
+api = Api()
+
+
+def configure_api(app):
+
+    # adicionamos a rota '/' a sua classe correspondente Index
+    api.add_resource(Index, '/')
+
+    # inicializamos a api com as configurações do flask vinda por parâmetro
+    api.init_app(app)
